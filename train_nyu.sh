d0747808885242736c8baa69b2b88c4452a07d1a
Ziyao Zeng <1147172798@qq.com>
2024-03-26 16:46:34 -0400

    first commit

--- train_nyu.sh
+++ train_nyu.sh
@@ -1,4 +1,4 @@
-exp_name="0322_0_wordepth_nyu_tmux2"
+model_name="0322_0_wordepth_nyu_tmux2"
 
-mkdir ./models/${exp_name}
-CUDA_VISIBLE_DEVICES=2 python src/train.py configs/arguments_train_nyu.txt  2>&1 | tee ./models/${exp_name}/result.log
+mkdir ./models/${model_name}
+CUDA_VISIBLE_DEVICES=2 python src/train.py configs/arguments_train_nyu.txt  2>&1 | tee ./models/${model_name}/result.log
